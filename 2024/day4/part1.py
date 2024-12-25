6e0a721414071038dc7b80fa4595fd6289ab21cd
guohaoice <guohaoice@gmail.com>
2024-12-26 06:11:55 +0800

    simplify

--- 2024/day4/part1.py
+++ 2024/day4/part1.py
@@ -6,7 +6,5 @@ g = {(i, j): ls[i][j] for i in range(len(ls)) for j in range(len(ls[i]))}
 t = 0
 for i, j in itertools.product(range(len(ls)), range(len(ls[0]))):
     for dx, dy in itertools.product(range(-1, 2), repeat=2):
-        line = ''.join(g.get((i + k * dx, j + k * dy), '.') for k in range(4))
-        if line == 'XMAS':
-            t += 1
+        t += 'XMAS' == ''.join(g.get((i + k * dx, j + k * dy), '.') for k in range(4))
 print(t)

84c35e787539b2fc1053512323d7d84d03886f48
gh <guohaoice@gmail.com>
2024-12-27 21:43:24 +0800

    06

--- 2015/06.py
+++ 2015/06.py
@@ -1,22 +1,22 @@
 from util import *
 
-g = Graph2D(rows=1000, cols=1000, default_val=0)
+g = [0] * 1000
 
 for line in L:
     xs, ys, xe, ye = ints(line)
-    for i in range(xs, xe + 1):
-        for j in range(ys, ye + 1):
-            if 'turn on' in line:
-                g[i, j] = 1
-            elif 'turn off' in line:
-                g[i, j] = 0
-            elif 'toggle' in line:
-                g[i, j] ^= 1
+    v = int('1' * (xe - xs + 1), 2) << xs
+    if 'turn on' in line:
+        f = lambda x: x | v
+    elif 'turn off' in line:
+        f = lambda x: ~(~x | v)
+    else:
+        f = lambda x: x ^ v
+    for i in range(ys, ye + 1):
+        g[i] = f(g[i])
 
-print(sum(g.values()))
+print(sum(x.bit_count() for x in g))
 
-g = Graph2D(rows=1000, cols=1000, default_val=0)
-
+g = defaultdict(int)
 for line in L:
     xs, ys, xe, ye = ints(line)
     for i in range(xs, xe + 1):

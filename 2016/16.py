0cc849e9f77ae5b6dd56fa27555cb954075bdd36
gh <guohaoice@gmail.com>
2024-12-29 13:16:45 +0800

    16

---
+++ 2016/16.py
@@ -0,0 +1,17 @@
+from util import *
+
+
+def f(n):
+    d = D
+    while len(d) < n:
+        d = d + '0' + re.sub(r'\d', lambda m: str(1 - int(m.group())), d[::-1])
+    d = d[:n]
+    while True:
+        d = re.sub(r'(\d)(\d)', lambda m: str(int(m.group(1) == m.group(2))), d)
+        if len(d) % 2:
+            print(d)
+            break
+
+
+f(272)
+f(35651584)

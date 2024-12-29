b8928d9c9112b1877c93b58fb52e14e347e05fd5
gh <guohaoice@gmail.com>
2024-12-29 12:21:00 +0800

    03

--- 2016/03.py
+++ 2016/03.py
@@ -1,5 +1,3 @@
-import itertools
-
 from util import *
 
 
@@ -11,7 +9,7 @@ def f(ns):
 print(count_nums(f))
 
 t = 0
-columns = list(itertools.chain.from_iterable(zip(*NS)))
+columns = list(chain.from_iterable(zip(*NS)))
 for i in range(0, len(columns), 3):
     nums = sorted(columns[i:i + 3])
     t += sum(nums[:2]) > nums[2]

7ae7b4b85f4e87c51de7d0e7ef8a70653fd86bb8
guohao <guohaoice@gmail.com>
2025-12-07 19:43:00 +0800

    06

--- 2025/06.py
+++ 2025/06.py
@@ -1,18 +1,10 @@
 import sys
 import re
+from itertools import groupby
 lines = [l[:-1] for l in sys.stdin.readlines()]
 print(sum(eval(l[-1].join(l[:-1])) for l in zip(*list(re.findall(r'\S+',line)  for line in lines))))
-ls =[''.join(l) for l in zip(*lines[:-1])]
-ss =re.findall(r'\S',lines[-1])
-i = 0
-r2 = 0 
-for s in ss:
-    j = i 
-    while i<len(ls) and ls[i].strip():
-        i+=1
-    r2+=eval(s.join(ls[j:i]))
-    i+=1
-print(r2)
+parts = [list(g) for k, g in groupby(list(''.join(l) for l in zip(*lines[:-1])), key=lambda s: bool(s.strip())) if k]
+print(sum(eval(s.join(p)) for s,p in zip(re.findall(r'\S',lines[-1]),parts)))
 
 
 

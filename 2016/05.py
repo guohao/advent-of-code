fd47a9bf380476a5f94c3104d71e6f31885f3b7a
guohao <guohaoice@gmail.com>
2025-12-10 14:35:38 +0800

    ruff

--- 2016/05.py
+++ 2016/05.py
@@ -1,11 +1,12 @@
 import sys
 from hashlib import md5
 from itertools import count
+
 D = input().strip()
 ans = ""
 
 for i in count():
-    s = md5((D+str(i)).encode()).hexdigest()[:6]
+    s = md5((D + str(i)).encode()).hexdigest()[:6]
     if s[:5] == "0" * 5:
         ans += s[-1]
         if len(ans) == 8:
@@ -14,11 +15,11 @@ print(ans)
 
 ans = ["x"] * 8
 for i in count():
-    s = md5((D+str(i)).encode()).hexdigest()[:7]
+    s = md5((D + str(i)).encode()).hexdigest()[:7]
     if s[:5] == "0" * 5:
         p = int(s[5], 16)
         if p < 8 and ans[p] == "x":
             ans[p] = s[6]
             if "x" not in ans:
-                print(''.join(ans))
+                print("".join(ans))
                 break
--- 2016/06.py
+++ 2016/06.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import sys
+
 L = sys.stdin.readlines()
 
-print(''.join(Counter(s).most_common(1)[0][0] for s in zip(*L)))
-print(''.join(Counter(s).most_common()[-1][0] for s in zip(*L)))
+print("".join(Counter(s).most_common(1)[0][0] for s in zip(*L)))
+print("".join(Counter(s).most_common()[-1][0] for s in zip(*L)))

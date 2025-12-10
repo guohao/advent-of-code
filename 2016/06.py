efb0997ab8744a968907d702dfb951d4007fba16
guohao <guohaoice@gmail.com>
2025-12-10 14:34:30 +0800

    06

--- 2016/06.py
+++ 2016/06.py
@@ -1,4 +1,6 @@
-from util import *
+from collections import Counter
+import sys
+L = sys.stdin.readlines()
 
-print(join(Counter(s).most_common(1)[0][0] for s in zip(*L)))
-print(join(Counter(s).most_common()[-1][0] for s in zip(*L)))
+print(''.join(Counter(s).most_common(1)[0][0] for s in zip(*L)))
+print(''.join(Counter(s).most_common()[-1][0] for s in zip(*L)))

118ef02de65d937c80dcbac4cf1bdef6552058d8
guohao <guohaoice@gmail.com>
2025-12-10 09:34:40 +0800

    09

--- 2016/01.py
+++ 2016/01.py
@@ -1,18 +1,32 @@
-from util import *
+import sys
+import re
+from itertools import combinations,product,permutations
+from collections import deque,defaultdict
+import heapq
+import math
 
-ans2 = None
+def turn(d,udlr):
+    dx,dy = d
+    match udlr:
+        case 'R':
+            dx,dy = dy,-dx
+        case 'L':
+            dx,dy = -dy,dx
+    return dx,dy
+
+r2 = None
 x = y = 0
 d = (1, 0)
 visited = {(x, y)}
-for step in D.split(", "):
-    d = turn(d, step[0])
+for step in input().strip().split(", "):
+    d = turn(d,step[0])
     moves = int(step[1:])
     for i in range(moves):
         x += d[0]
         y += d[1]
         if (x, y) in visited:
-            if not ans2:
-                ans2 = abs(x) + abs(y)
+            if not r2:
+                r2 = abs(x) + abs(y)
         visited.add((x, y))
 print(abs(x) + abs(y))
-print(ans2)
+print(r2)
---
+++ 2025/09.py
@@ -0,0 +1,78 @@
+import sys
+import re
+from itertools import combinations, product, permutations, chain
+from collections import deque, defaultdict
+from functools import cache
+import heapq
+import math
+
+g = []
+prev = None
+for l in sys.stdin.readlines():
+    x, y = map(int, l.strip().split(","))
+    g.append((x, y))
+
+h_edges = []
+v_edges = []
+for ia in range(len(g)):
+    a = g[ia - 1]
+    b = g[ia]
+    a, b = sorted([a, b])
+    if a[0] == b[0]:
+        v_edges.append((a, b))
+    else:
+        h_edges.append((a, b))
+g = set(g)
+
+
+def p_in(x, y):
+    if (x, y) in g:
+        return True
+    udlr = set()
+    for (x0, hy), (x1, _) in h_edges:
+        if x0 <= x <= x1:
+            if y == hy:
+                return True
+            elif y < hy:
+                udlr.add("U")
+            else:
+                udlr.add("D")
+    for (vx, y0), (_, y1) in v_edges:
+        if y0 <= y <= y1:
+            if x == vx:
+                return True
+            elif x < vx:
+                udlr.add("L")
+            else:
+                udlr.add("R")
+    return len(udlr) == 4
+
+
+def line_in(x, y1, y2):
+    y1, y2 = sorted([y1, y2])
+    for (x0, y), (x1, _) in h_edges:
+        if x0 < x < x1 and y1 < y < y2:
+            return False
+    return True
+
+
+r1 = 0
+
+
+def area(a, b):
+    return (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1)
+
+
+for a, b in combinations(g, 2):
+    r1 = max(area(a, b), r1)
+print(r1)
+
+r2 = 0
+for a, b in combinations(g, 2):
+    (ax, ay), (bx, by) = sorted([a, b])
+    cx, cy = ax, by
+    dx, dy = bx, ay
+    if p_in(cx, cy) and p_in(dx, dy):
+        if line_in(ax, ay, by):
+            r2 = max(area(a, b), r2)
+print(r2)

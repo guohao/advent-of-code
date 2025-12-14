314411cc749305611b1676aa2152847c64d145ac
guohao <guohaoice@gmail.com>
2025-12-14 18:45:58 +0800

    01

--- 2024/01.py
+++ 2024/01.py
@@ -1,10 +1,7 @@
-from collections import Counter
 import sys
-import re
 
-NS = [list(map(int, re.findall(r"\d+", line))) for line in sys.stdin.readlines()]
-print(sum(abs(a - b) for a, b in zip(*map(sorted, zip(*NS)))))
-
-a, b = zip(*NS)
-b = Counter(b)
-print(sum(x * b[x] for x in a))
+NS = list(map(int, sys.stdin.read().split()))
+a = sorted(NS[0::2])
+b = sorted(NS[1::2])
+print(sum(abs(a - b) for a, b in zip(a, b)))
+print(sum(x * b.count(x) for x in a))
